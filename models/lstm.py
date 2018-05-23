c0fa7fff0b7a705dcd0fb32dea630ea69cf8dfb8
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-05-23 16:28:59 +0200

    refactor

--- models/lstm.py
+++ models/lstm.py
@@ -20,11 +20,11 @@ torch.manual_seed(1983)
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
-def embedding_layer(weights, n_padding_vectors=2, trainable=False):
+def embedding_layer(weights, n_padding_vectors=4, trainable=False):
     """Create an embedding layer from pre-trained gensim embeddings."""
     weights = np.vstack((np.zeros((n_padding_vectors, weights.shape[1])), weights))
-    embedding_weights = torch.FloatTensor(weights)
-    embedding = torch.nn.Embedding(*embedding_weights.shape, padding_idx=0)
+    embedding_weights = torch.tensor(weights, dtype=torch.float32)
+    embedding = torch.nn.Embedding(*embedding_weights.shape, padding_idx=PAD)
     embedding.weight = torch.nn.Parameter(embedding_weights, requires_grad=trainable)
     return embedding
 
@@ -37,45 +37,85 @@ def load_gensim_embeddings(fpath: str):
 
 class LSTMTagger(torch.nn.Module):
     def __init__(self, embedding_dim, hidden_dim, num_layers, embedding_dropout_p,
-                 stress_size, pos_size, syllable_encoder, tagset_size, batch_size):
-        super(LSTMTagger, self).__init__()
+                 stress_size, wb_size, syllable_encoder, tagset_size, batch_size,
+                 bidirectional=True):
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.batch_size = batch_size
-        self.stress_encoder = torch.nn.Embedding(stress_size, embedding_dim, padding_idx=0)
-        self.wb_encoder = torch.nn.Embedding(3, embedding_dim, padding_idx=0)
+        self.embedding_dropout_p = embedding_dropout_p
+        super(LSTMTagger, self).__init__()
+
+        # loss weight to remove padding
+        weight = torch.ones(tagset_size)
+        weight[BOS] = 0.
+        self.register_buffer('weight', weight)
+        self.stress_encoder = torch.nn.Embedding(
+            stress_size, embedding_dim, padding_idx=PAD)
+        self.wb_encoder = torch.nn.Embedding(wb_size, embedding_dim, padding_idx=PAD)
         self.syllable_encoder = syllable_encoder
-        self.dropout = torch.nn.Dropout(p=embedding_dropout_p)
+
+        num_dirs = 1 + int(bidirectional)
         self.lstm = torch.nn.LSTM(
             embedding_dim * 2 + syllable_encoder.weight.shape[1],
-            hidden_dim, num_layers=num_layers, batch_first=True)
+            hidden_dim // num_dirs, num_layers=num_layers, batch_first=True,
+            bidirectional=True)
+
         self.hidden2tag = torch.nn.Linear(hidden_dim, tagset_size)
 
-    def forward(self, stress_sequence, wb_sequence, syllable_sequence, perm_index, lengths):
-        stress_embeddings = self.stress_encoder(stress_sequence)
-        wb_embeddings = self.wb_encoder(wb_sequence)
-        syllable_embeddings = self.syllable_encoder(syllable_sequence)
-        embeddings = self.dropout(torch.cat((stress_embeddings, wb_embeddings, syllable_embeddings), 2))
-        embeddings = embeddings[perm_index]
-        packed = torch.nn.utils.rnn.pack_padded_sequence(embeddings, lengths, batch_first=True)
-        lstm_out, _ = self.lstm(embeddings)
+    def forward(self, stress_sequence, wb_sequence, syllable_sequence, lengths):
+        stress_embs = self.stress_encoder(stress_sequence)
+        wb_embs = self.wb_encoder(wb_sequence)
+        syllable_embs = self.syllable_encoder(syllable_sequence)
+        embs = torch.cat([stress_embs, wb_embs, syllable_embs], 2)
+        embs = torch.nn.functional.dropout(
+            embs, p=self.embedding_dropout_p, training=self.training)
+        # packed = torch.nn.utils.rnn.pack_padded_sequence(
+        #     embs, lengths, batch_first=True)
+        lstm_out, _ = self.lstm(embs)
         tag_space = self.hidden2tag(lstm_out)
-        tag_scores = torch.nn.functional.log_softmax(tag_space, dim=1)
-        return tag_scores
+
+        return tag_space
+
+    def loss(self, stress_sequence, wb_sequence, syllable_sequence, lengths, targets):
+        tag_space = self(stress_sequence, wb_sequence, syllable_sequence, lengths)
+        loss = torch.nn.functional.cross_entropy(
+            tag_space.view(-1, tag_space.size(2)), targets.view(-1),
+            weight=self.weight, size_average=False)
+        loss = loss / lengths.sum().item()
+
+        return loss
+
+    def predict(self, stress_sequence, wb_sequence, syllable_sequence, lengths):
+        tag_space = self(stress_sequence, wb_sequence, syllable_sequence, lengths)
+        # tag_scores = torch.nn.functional.log_softmax(tag_space, dim=1)
+        pred = tag_space.view(-1, tag_space.size(2)).data.cpu().numpy().argmax(1)
+
+        return pred
 
 
-class BiLSTMTagger(LSTMTagger):
-    def __init__(self, embedding_dim, hidden_dim, num_layers, embedding_dropout_p,
-                 stress_size, pos_size, syllable_encoder, tagset_size, batch_size):
-        super(BiLSTMTagger, self).__init__(
-            embedding_dim, hidden_dim, num_layers,
-            embedding_dropout_p, stress_size, pos_size,
-            syllable_encoder, tagset_size, batch_size)
+class CRFLSTM(LSTMTagger):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.lstm = torch.nn.LSTM(
-            embedding_dim * 2 + syllable_encoder.weight.shape[1], hidden_dim // 2,
-            num_layers =num_layers, bidirectional=True, batch_first=True, # dropout=0.5
-        )
+        # matrix of transition scores from j to i
+        trans = torch.randn(vocab, vocab)
+        trans[BOS, :] = -10000. # no transition to <bos>
+        trans[:, EOS] = -10000. # no transition from <eos> except to <pad>
+        trans[:, PAD] = -10000. # no transition from <pad> except to <pad>
+        trans[PAD, :] = -10000.
+        trans[PAD, EOS] = 0.
+        trans[PAD, PAD] = 0.
+        self.trans = nn.Parameter(trans)
+
+    def forward(self, stress_sequence, wb_sequence, syllable_sequence, lengths):
+        tag_space = super().forward(
+            stress_sequence, wb_sequence, syllable_sequence, lengths)
+
+    def loss(self):
+        pass
+
+    def predict(self):
+        pass
 
 
 Verse = collections.namedtuple('Verse', 'stress, wb, syllables, beats, beatstress')
@@ -145,28 +185,42 @@ def indexer(pre_fill=None):
     return d
 
 
+PAD = 0
+BOS = 1
+EOS = 2
+UNK = 3  # if it exists
+
+
 class Sample2Tensor:
     def __init__(self, syllable_vocab, max_input_len=30, padding_char='<PAD>'):
         self.max_input_len = max_input_len
-        self.stress_index = indexer(pre_fill=(padding_char,))
-        self.pos_index = indexer(pre_fill=(padding_char,))
-        self.syllable_index = indexer(pre_fill=(padding_char, '<UNK>') + tuple(syllable_vocab))
-        self.beat_index = indexer(pre_fill=(padding_char,))
-        self.target_index = indexer(pre_fill=(padding_char,))
+        self.stress_index = indexer(pre_fill=(padding_char, '<BOS>', '<EOS>'))
+        # self.pos_index = indexer(pre_fill=(padding_char,))
+        self.wb_index = indexer(pre_fill=(padding_char, '<BOS>', '<EOS>'))
+        self.syllable_index = indexer(pre_fill=(padding_char, '<BOS>', '<EOS>', '<UNK>') + tuple(syllable_vocab))
+        self.target_index = indexer(pre_fill=(padding_char, '<BOS>', '<EOS>'))
 
     def __call__(self, sample: Verse):
+        # to integers
         word_stress = [self.stress_index[x] for x in sample.stress]
-        word_boundaries = [b + 1 for b in sample.wb]
+        word_boundaries = [self.wb_index[b] for b in sample.wb]
         syllables = [self.syllable_index.get(syllable.lower(), self.syllable_index['<UNK>'])
                      for syllable in sample.syllables]
         beat_stress = [self.target_index[beat] for beat in sample.beatstress]
-        # beat_stress = [self.beat_index[beat] for beat in sample[3].split()]
+        # to sequences
+        word_stress = self._pad_sequence(word_stress, self.stress_index)
+        word_boundaries = self._pad_sequence(word_boundaries, self.wb_index)
+        syllables = self._pad_sequence(syllables, self.syllable_index)
+        beat_stress = self._pad_sequence(beat_stress, self.target_index)
+        # length
         length = len(word_stress)
+        # padding
         while len(word_stress) < self.max_input_len:
             word_stress.append(0)
             syllables.append(0)
             word_boundaries.append(0)
             beat_stress.append(0)
+
         return {'stress': torch.LongTensor(word_stress),
                 'syllables': torch.LongTensor(syllables),
                 'wb': torch.LongTensor(word_boundaries),
@@ -194,14 +248,13 @@ class Sample2Tensor:
 
 
 class Trainer:
-    def __init__(self, model: BiLSTMTagger, train_data: DataLoader,
+    def __init__(self, model: LSTMTagger, train_data: DataLoader,
                  dev_data: DataLoader = None, test_data: DataLoader = None,
-                 optimizer=None, loss_fn=None, device=None, decoder=None):
+                 optimizer=None, device=None, decoder=None):
         self.logger = logging.getLogger('Trainer({})'.format(model.__class__.__name__))
         self.model = model
         self.optimizer = optimizer
         self.device = device
-        self.loss_fn = loss_fn
         self.train_data = train_data
         self.dev_data = dev_data
         self.test_data = test_data
@@ -231,13 +284,15 @@ class Trainer:
             syllables = batch['syllables'].to(self.device)
             targets = batch['tags'].to(self.device)
             lengths, perm_index = batch['length'].sort(0, descending=True)
+
+            stress = stress[perm_index]
+            wb = wb[perm_index]
+            syllables = syllables[perm_index]
+            targets = targets[perm_index]
             
             self.model.zero_grad()
 
-            tag_scores = self.model(stress, wb, syllables, perm_index, lengths)
-            targets = targets[perm_index]
-            loss = self.loss_fn(tag_scores.view(-1, tag_scores.size(2)), targets.view(-1))
-            loss = loss / lengths.sum().item()
+            loss = self.model.loss(stress, wb, syllables, lengths, targets)
             loss.backward()
             self.optimizer.step()
             loss = loss.item()
@@ -259,31 +314,29 @@ class Trainer:
             wb = batch['wb'].to(self.device)
             syllables = batch['syllables'].to(self.device)
             targets = batch['tags'].to(self.device)
-            lengths, perm_index = batch['length'].sort(0, descending=True)            
-            
-            self.model.zero_grad()
-            
-            tag_scores = self.model(stress, wb, syllables, perm_index, lengths)
+            lengths, perm_index = batch['length'].sort(0, descending=True)
+            stress = stress[perm_index]
+            wb = wb[perm_index]
+            syllables = syllables[perm_index]
             targets = targets[perm_index]
 
             if not test:
-                loss = self.loss_fn(
-                    tag_scores.view(-1, tag_scores.size(2)), targets.view(-1)).item()
+                loss = self.model.loss(stress, wb, syllables, lengths, targets)
                 loss = loss / lengths.sum().item()
                 run_loss += loss
 
             # collect predictions
-            pred = tag_scores.view(-1, tag_scores.size(2)).data.cpu().numpy().argmax(1)
-            true = targets.view(-1).data.cpu().numpy()
+            pred = self.model.predict(stress, wb, syllables, lengths)
+            true = targets.view(-1).cpu().numpy()
             y_true.append(true)
             y_pred.append(pred)
-            true = true.reshape(tag_scores.shape[0], stress.size(1))
-            pred = pred.reshape(tag_scores.shape[0], stress.size(1))
+            true = true.reshape(data.batch_size, stress.size(1))
+            pred = pred.reshape(data.batch_size, stress.size(1))
             accuracy += (true == pred).all(1).sum() / stress.size(0)
             baccuracy += ((true > 0) == (pred > 0)).all(1).sum() / stress.size(0)
             if i % 10 == 0:
                 syllables = syllables[perm_index]
-                for elt in np.random.randint(0, tag_scores.shape[0], 2):
+                for elt in np.random.randint(0, data.batch_size, 2):
                     self.decoder.decode(syllables[elt], true[elt], pred[elt])
         if not test:
             logging.info('Validation Loss: {}'.format(run_loss / len(data)))
@@ -354,27 +407,23 @@ if __name__ == '__main__':
     dev_batches = torch.utils.data.DataLoader(dev, batch_size=batch_size, shuffle=True)
     test_batches = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=True)
     # construct a layer holding the pre-trained word2vec embeddings
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")        
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     syllable_encoder = embedding_layer(
         syllable_vectors, trainable=args.retrain_embeddings)
     # Initialize the tagger
-    tagger = BiLSTMTagger(
+    tagger = LSTMTagger(
         args.emb_dim, args.hid_dim, args.num_layers, args.dropout, 5,
-        40, syllable_encoder, 3, batch_size)
-    # define the loss function. We choose CrossEntropyloss
-    weight = torch.ones(3).to(device)
-    weight[0] = 0.
-    loss_function = torch.nn.CrossEntropyLoss(size_average=False, weight=weight)
+        5, syllable_encoder, 5, batch_size, bidirectional=True)
+    print(tagger)
     # The Adam optimizer seems to be working fine. Make sure to exlcude the pretrained
     # embeddings from optimizing
     optimizer = torch.optim.Adam(
         filter(lambda p: p.requires_grad, tagger.parameters()),
         lr=args.learning_rate, weight_decay=args.weight_decay
-    ) #RMSprop
+    ) # RMSprop
     tagger.to(device)
     trainer = Trainer(
         tagger, train_batches, dev_batches, test_batches, optimizer,
-        loss_function, decoder=transformer, device=device
-    )
+        decoder=transformer, device=device)
     trainer.train(epochs=args.epochs)
     trainer.test(statefile='model_best.pth.tar')

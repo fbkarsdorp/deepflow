3053a27089fddb2111afc36a25ad325205d9f622
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-05-25 23:23:38 +0200

    fix

--- models/loaders.py
+++ models/loaders.py
@@ -18,33 +18,71 @@ def identity(x): return x
 def word_boundaries(syllables):
     return [1] + [0] * (len(syllables) - 1)
 
+def normalize_stress(stress):
+    return [int(s) if s != '.' else 0 for s in stress]
+
 
 class Encoder:
     def __init__(self, name, pad_token=PAD, eos_token=EOS, bos_token=BOS, unk_token=UNK,
-                 vocab=None, preprocessor=identity):
+                 vocab=None, fixed_vocab=False, preprocessor=identity):
         self.index = collections.defaultdict()
         self.index.default_factory = lambda: len(self.index)
         self.pad_token = pad_token
         self.eos_token = eos_token
         self.bos_token = bos_token
         self.unk_token = unk_token
+        self.reserved_tokens = (pad_token, eos_token, bos_token, unk_token)
         self.name = name
         self.preprocessor = preprocessor
+        self.fixed_vocab = fixed_vocab
 
-        for token in (pad_token, eos_token, bos_token, unk_token):
+        for token in self.reserved_tokens:
             self.index[token]
         if vocab is not None:
             for token in vocab:
                 self.index[token]
 
+    def __getitem__(self, item):
+        if self.fixed_vocab:
+            return self.index.get(item, self.unk_index)
+        return self.index[item]
+
     def __len__(self):
         return len(self.index)
 
+    def size(self):
+        return len(self)
+
+    @property
+    def pad_index(self):
+        return self.index[self.pad_token]
+
+    @property
+    def bos_index(self):
+        return self.index[self.bos_token]
+
+    @property
+    def eos_index(self):
+        return self.index[self.eos_token]
+
+    @property
+    def unk_index(self):
+        return self.index[self.unk_token]
+
+    def __repr__(self):
+        return '<Encoder({})>'.format(self.name)
+
     def transform(self, sample, max_seq_len):
-        eos = [self.index[self.eos_token]] if self.eos_token is not None else []
-        sample = [self.index[elt] for item in sample for elt in self.preprocessor(item[self.name])] + eos
-        sample = sample + [self.index[self.pad_token]] * (max_seq_len - len(sample) - 1)
+        eos = [self.eos_index] if self.eos_token is not None else []
+        sample = [self[elt] for item in sample for elt in self.preprocessor(item[self.name])] + eos
+        sample = sample + [self.pad_index] * (max_seq_len - len(sample) - 1)
         return sample
+
+    def decode(self, sample):
+        if not hasattr(self, 'index2item'):
+            self.index2item = sorted(self.index, key=self.index.get)
+        return [self.index2item[elt] for elt in sample]
+        
 
     
 class DataSet:

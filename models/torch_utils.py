871e0259afd14b2d15b40463756ff297ce59c950
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-05-29 17:28:21 +0200

    remove unused stuff

--- models/torch_utils.py
+++ models/torch_utils.py
@@ -19,23 +19,3 @@ def make_length_mask(lengths, maxlen=None, device='cpu'):
     return torch.arange(0, maxlen, dtype=torch.int64, device=device) \
                 .repeat(batch, 1) \
                 .lt(lengths.to(device).unsqueeze(1))
-
-
-def log_sum_exp(x):
-    """
-    Numerically stable log_sum_exp
-
-    Parameters
-    ==========
-    x : torch.tensor
-
-    >>> import torch
-    >>> x = torch.randn(10, 5)
-    """
-    max_score, _ = torch.max(x, -1)
-    max_score_broadcast = max_score.unsqueeze(-1).expand_as(x)
-    return max_score + torch.log(torch.sum(torch.exp(x - max_score_broadcast), -1))
-
-
-def prepad(x, pad=0):
-    return torch.nn.functional.pad(x, (1, 0), value=pad)

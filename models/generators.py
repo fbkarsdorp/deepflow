a32a357d121ffd888f257c246a6ef146b732f616
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-05-30 15:05:43 +0200

    added generation code

--- models/generators.py
+++ models/generators.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 
 import torch_utils
 import loaders
+from lstm import embedding_layer
 
 
 def trim_hidden(hidden, new_lengths):
@@ -16,7 +17,8 @@ def trim_hidden(hidden, new_lengths):
             return None
 
         # remove finished batch song from hidden
-        return hidden[0][:,:len(new_lengths)].contiguous(), hidden[1][:,:len(new_lengths)].contiguous()
+        return (hidden[0][:,:len(new_lengths)].contiguous(),
+                hidden[1][:,:len(new_lengths)].contiguous())
 
     return hidden
 
@@ -59,6 +61,9 @@ class LM(nn.Module):
             self.proj.weight = self.embs.weight
 
         self.init()
+
+    def device(self):
+        return next(self.parameters()).device
 
     def init(self):
         initrange = 0.1
@@ -111,8 +116,8 @@ class LM(nn.Module):
 
         return total_loss / total_batches
 
-    def train_model(self, epochs, dataset, optim, device, devset=None, report_freq=100,
-                    max_norm=None):
+    def train_model(self, epochs, dataset, optim, device, devset=None, max_norm=None,
+                    report_freq=1000, check_freq=4000, encoder=None):
 
         for e in range(epochs):
             print("Training on epoch [{}]".format(e+1))
@@ -146,10 +151,24 @@ class LM(nn.Module):
                 total_batches += 1
                 epoch_batches += 1
 
-                if batch_num > 0 and batch_num + 1 % report_freq == 0:
+                if batch_num > 0 and (batch_num + 1) % report_freq == 0:
                     print("Epoch/batch: {}/{}: {:.3f} ppl".format(
                         e+1, batch_num+1, total_loss / total_batches))
                     total_loss, total_batches = 0, 0
+
+                if batch_num > 0 and (batch_num + 1) % check_freq == 0:
+                    if devset is not None:
+                        print("Evaluating...")
+                        with torch.no_grad():
+                            self.eval()
+                            dev_loss = self.evaluate(devset, device)
+                            self.train()
+                        print("Dev loss: {:.3f}".format(dev_loss))
+
+                    if encoder is not None:
+                        print("Generating samples:")
+                        for hyp, score in zip(*self.generate(encoder)):
+                            print('[{:.2f}] => {}'.format(score, ' '.join(hyp)))
 
             print("Epoch loss: {:.3f}".format(epoch_loss / epoch_batches))
 
@@ -161,6 +180,51 @@ class LM(nn.Module):
                     self.train()
                 print("Dev loss: {:.3f}".format(dev_loss))
 
+    def generate(self, encoder, nlines=5, maxlen=25, sample=True, temp=1.0):
+        # input variables
+        inp = torch.tensor([encoder.bos_index] * nlines).to(self.device())
+        lengths = torch.ones(nlines, dtype=torch.int64).to(self.device())
+        hidden = None
+        # accumulators
+        mask = torch.ones(nlines, dtype=torch.int64)
+        scores, hyps = 0, []
+
+        for _ in range(maxlen):
+            # check if finished
+            if sum(mask).item() == 0:
+                break
+
+            # run forward
+            inp = inp.unsqueeze(1)  # add seq_len dim
+            preds, hidden = self(inp, lengths, hidden)
+            preds = preds.squeeze(1)  # remove seq_len dim
+
+            # sample
+            preds = F.log_softmax(preds, dim=-1)
+            if sample:
+                inp = (preds/temp).exp().multinomial(1)
+                score = preds.gather(1, inp)
+                score, inp = score.squeeze(1), inp.squeeze(1)
+            else:
+                score, inp = torch.max(preds, dim=-1)
+
+            # accumulate
+            mask = mask * (inp.cpu() != encoder.eos_index).long()
+            scores += score.cpu() * mask.float()
+            hyp = inp.tolist()
+            for idx, active in enumerate(mask.tolist()):
+                if not active:
+                    hyp[idx] = None
+            hyps.append(hyp)
+
+        # get results
+        hyps = [list(filter(None, hyp)) for hyp in zip(*hyps)]
+        hyps = [encoder.decode(hyp) for hyp in hyps]
+        scores = scores.tolist()
+        scores = [score/(len(hyp) + 1e-25) for score, hyp in zip(scores, hyps)]
+
+        return hyps, scores
+
 
 if __name__ == '__main__':
     import argparse
@@ -170,32 +234,43 @@ if __name__ == '__main__':
     parser.add_argument('--hid_dim', default=200, type=int)
     parser.add_argument('--dropout', default=0.5, type=float)
     parser.add_argument('--epochs', default=100, type=int)
-    parser.add_argument('--batch_size', default=20, type=int)
+    parser.add_argument('--batch_size', default=50, type=int)
     parser.add_argument('--train_file', required=True, type=str)
     parser.add_argument('--dev_file', required=True, type=str)
     parser.add_argument('--pretrained_embeddings', required=True, type=str)
-    parser.add_argument('--learning_rate', default=10., type=float)
-    parser.add_argument('--max_norm', default=0.25, type=float)
+    parser.add_argument('--learning_rate', default=0.0003, type=float)
+    parser.add_argument('--max_norm', default=5, type=float)
     parser.add_argument('--lr_patience', type=int, default=5)
     args = parser.parse_args()
 
+    # data loading
     eos, bos = '<EOS>', '<BOS>'
     syllable_vocab, syllable_vectors = loaders.load_gensim_embeddings(
         args.pretrained_embeddings)
     syllable_encoder = loaders.Encoder(
         'syllables', vocab=syllable_vocab, fixed_vocab=True,
         eos_token=eos, bos_token=bos)
-    
-    trainset = loaders.BlockDataSet(args.train_file, batch_size=args.batch_size,
-                                    syllables=syllable_encoder)
-    devset = loaders.BlockDataSet(args.dev_file, batch_size=args.batch_size,
-                                  syllables=syllable_encoder)
-    model = LM(len(syllable_vocab), args.emb_dim, args.hid_dim, args.num_layers,
+    trainset = loaders.BlockDataSet(
+        args.train_file, batch_size=args.batch_size, syllables=syllable_encoder)
+    devset = loaders.BlockDataSet(
+        args.dev_file, batch_size=args.batch_size, syllables=syllable_encoder)
+
+    # model
+    model = LM(len(syllable_encoder), args.emb_dim, args.hid_dim, args.num_layers,
                dropout=args.dropout, padding_idx=syllable_encoder.pad_index)
     print(model)
-    optim = torch.optim.SGD(model.parameters(), lr=args.learning_rate)
 
+    # initialize embeddings
+    syllable_embs = embedding_layer(
+        syllable_vectors, n_padding_vectors=len(syllable_encoder.reserved_tokens),
+        padding_idx=syllable_encoder.pad_index, trainable=True)
+    if syllable_embs.weight.size() == model.embs.weight.size():
+        model.embs.weight = syllable_embs.weight
+
+    # training
+    optim = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     model.train_model(args.epochs, trainset, optim, device,
-                      devset=devset, max_norm=args.max_norm)
+                      devset=devset, max_norm=args.max_norm,
+                      encoder=syllable_encoder)

45b951a7e0d53e553a565a514cb67417904395ea
BSTN <BSTN@users.noreply.github.com>
2018-08-09 13:57:31 +0100

    update lyrics frontend resample

--- api/app/static/lyrics/script.js
+++ api/app/static/lyrics/script.js
@@ -45,8 +45,10 @@ var app = new Vue({
       this.$forceUpdate()
       document.activeElement.blur()
       this.storeInBrowser()
+      this.generate()
     },
-    generate () {
+    generate (resample) {
+      if (resample === undefined) resample = false
       if (this.storage.lyric.length < 20) {
         if (!this.loading) {
           let self = this
@@ -54,11 +56,11 @@ var app = new Vue({
           self.loading = true
           self.log('generate')
           // send seed id
-          let id = null
-          if (this.storage.lyric.length > 0) {
-            id = this.storage.lyric[this.storage.lyric.length - 1].id
-          }
-          axios.post(self.generateUrl,{seed_id: id}).then(function(res) {
+          // let id = null
+          // if (this.storage.lyric.length > 0) {
+          //   id = this.storage.lyric[this.storage.lyric.length - 1].id
+          // }
+          axios.post(self.generateUrl,{resample: resample}).then(function(res) {
             if (res.data.id) {
               self.id = res.data.id
               self.log('received job id', {jobid: res.data.id})
@@ -140,6 +142,11 @@ var app = new Vue({
         }
       }
     },
+    switchcensor () {
+      this.censored = !this.censored
+      if (this.censored) this.log('censor true', this.id)
+      else this.log('censor false', this.id)
+    },
     storeInBrowser () {
       localStorage.setItem('storage', JSON.stringify(this.storage));
     },
@@ -156,13 +163,13 @@ var app = new Vue({
     let self = this
     let ls = localStorage.getItem('storage')
     this.storage = (ls !== '' && ls !== null) ? JSON.parse(ls) : this.storage
-    // this.generate()
+    if (this.storage.lyric.generated.length < 1) this.generate()
     setTimeout(this.show, 500)
     window.addEventListener('keydown', function(ev) {
       if (ev.keyCode === 40) self.changeSelection(-1)
       if (ev.keyCode === 38) self.changeSelection(1)
       if (ev.keyCode === 13) self.add(self.last[self.selected])
-      if (ev.keyCode === 32) self.generate()
+      if (ev.keyCode === 32) self.generate(true)
       if (ev.keyCode === 27) self.submit()
     })
   }

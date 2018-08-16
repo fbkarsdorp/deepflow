13bd0fc872380cafb31e9d14378bf6d528b6886a
BSTN <BSTN@users.noreply.github.com>
2018-08-16 13:45:30 +0100

    landing change

--- api/app/static/landing/script.js
+++ api/app/static/landing/script.js
@@ -6,4 +6,19 @@ var app = new Vue({
       media: false
     }
   },
+  created () {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      var self = this
+      axios.get('/static/media.json').then(function(res){
+        if (res.data) {
+          self.media = res.data.media
+        }
+      })
+    }
+  }
 });

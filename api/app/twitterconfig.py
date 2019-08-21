b291099c24def767990169598bb933039bd0df4f
Folgert Karsdorp <fbkarsdorp@gmail.com>
2019-08-21 17:09:37 +0200

    fix

--- api/app/twitterconfig.py
+++ api/app/twitterconfig.py
@@ -1,6 +1,6 @@
 
-consumer_key = 'hMRzcdhKFSMVXFPSLM5zPwexy'
-consumer_secret = 'GoseFMrJEFxZU7WcLrHLwUH52kDETac2UgRQvXbjK3yyiLooXv'
-access_token = '1023937768270036992-CVcUjySzpqtNNUHeJtoqJODLfu1NYm'
-access_secret = 'boj2RW3hqxvfggDYqNmUJKv8ee9Vh8QbtfoRGVAzjg7El'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
